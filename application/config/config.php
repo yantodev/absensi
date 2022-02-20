d6afb23b1bc55f60264876f80f6661ff4cc821e1
yanto <ekocahyanto007@gmail.com>
2022-02-20 17:02:14 +0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -372,7 +372,7 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 3600;
+$config['sess_expiration'] = 36000;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
